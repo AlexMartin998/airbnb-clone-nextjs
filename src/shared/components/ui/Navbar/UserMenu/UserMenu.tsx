ae67ba9e157d01847a5b88d140eeccac760cb0aa
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-05 16:09:33 -0500

    [ADD]: UserMenu component

---
+++ src/shared/components/ui/Navbar/UserMenu/UserMenu.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+import { Avatar, MenuLink } from '../..';
+import { navLinks } from './navLinks';
+
+export type UserMenuProps = {};
+
+const UserMenu: React.FC<UserMenuProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen(prev => !prev);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          Airbnb your home
+        </div>
+
+        {/* dropdown menu */}
+        <button
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[30vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            {navLinks.map(({ label, path }) => (
+              <MenuLink key={path} label={label} path={path} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
--- src/shared/components/ui/Navbar/index.ts
+++ src/shared/components/ui/Navbar/index.ts
@@ -1,3 +1,7 @@
 export { default as Logo } from './Logo/Logo';
+export { default as MenuItem } from './MenuItem/MenuItem';
+export { default as MenuLink } from './MenuLink/MenuLink';
 export { default as Navbar } from './Navbar';
 export { default as Search } from './Search/Search';
+export { default as UserMenu } from './UserMenu/UserMenu';
+export * from './UserMenu/navLinks';
