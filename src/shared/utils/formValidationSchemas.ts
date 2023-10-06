40afcf501bf5aac931678562fe14f79fa1c386d1
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-05 22:31:16 -0500

    [ADD]: Schema validator changed to yup

--- src/shared/utils/formValidationSchemas.ts
+++ src/shared/utils/formValidationSchemas.ts
@@ -1,15 +1,27 @@
-import { email, minLength, object, string } from 'valibot';
+import * as yup from 'yup';
 
-export const registerFormSchema = object({
-  name: string('Name is required', [
-    minLength(3, 'Needs to be at least 3 characters'),
-  ]),
-  email: string('Email is required', [
-    minLength(3, 'Needs to be at least 3 characters'),
-    email('Invalid email'),
-  ]),
-  password: string('Password is required', [
-    minLength(6, 'Needs to be at least 6 characters'),
-    email('Invalid email'),
-  ]),
+const emailYupValidation = yup
+  .string()
+  .matches(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    'Invalid email'
+  )
+  .required('Email is requiered')
+  .min(2, 'Min 2 characteres')
+  .max(24, 'Max 12 characteres');
+
+const passwordYupValidation = yup
+  .string()
+  .required('Password is requiered')
+  .min(2, 'Min 2 characteres')
+  .max(33, 'Max 12 characteres');
+
+export const registerFormSchema = yup.object({
+  name: yup
+    .string()
+    .required('Name is requiered')
+    .min(2, 'Min 2 characteres')
+    .max(30, 'Max 12 characteres'),
+  email: emailYupValidation,
+  password: passwordYupValidation,
 });
