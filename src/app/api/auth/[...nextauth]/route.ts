70658a7d23e43254d907f7adb08bc5e0200ac72d
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-07 16:50:38 -0500

    [ADD]: NextAuth initial setup + GitHub/Google Provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,70 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt from 'bcrypt';
+import { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { db } from '@/shared/lib/prismadb';
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db),
+
+  providers: [
+    // social login
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+
+    // credentials
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+
+      // login & register pass through here
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword)
+          throw new Error('Invalid credentials');
+        if (!bcrypt.compareSync(credentials.password, user.hashedPassword!))
+          return null;
+
+        return user;
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: '/', // 'cause we don't use login page, but a modal
+  },
+
+  // show errs in dev mode
+  debug: process.env.NODE_ENV === 'development',
+
+  session: {
+    strategy: 'jwt',
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/shared/lib/prismadb.ts
+++ src/shared/lib/prismadb.ts
@@ -3,10 +3,17 @@ import { PrismaClient } from '@prisma/client';
 // // avoit creating multiple PrismaClient instances by hot reloading
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var cachedPrisma: PrismaClient;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
 
-export default client;
+export const db = prisma;
