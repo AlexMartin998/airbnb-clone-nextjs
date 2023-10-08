d66a880d2696d7318cab25692c668a0da3fde5ba
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-08 12:38:28 -0500

    [ADD]: CountrySelect input ui component

--- src/shared/components/ui/Inputs/index.ts
+++ src/shared/components/ui/Inputs/index.ts
@@ -1,2 +1,3 @@
 export { default as CategoryInput } from './CategoryInput/CategoryInput';
+export { default as CountrySelect } from './CountrySelect/CountrySelect';
 export { default as Input } from './Input/Input';
