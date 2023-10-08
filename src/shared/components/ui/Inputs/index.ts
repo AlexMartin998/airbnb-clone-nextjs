890703a65bc9e9be0485bc689135af5fbee6e601
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-08 15:02:25 -0500

    [ADD]: Counter

--- src/shared/components/ui/Inputs/index.ts
+++ src/shared/components/ui/Inputs/index.ts
@@ -1,3 +1,4 @@
 export { default as CategoryInput } from './CategoryInput/CategoryInput';
+export { default as Counter } from './Counter/Counter';
 export { default as CountrySelect } from './CountrySelect/CountrySelect';
 export { default as Input } from './Input/Input';
