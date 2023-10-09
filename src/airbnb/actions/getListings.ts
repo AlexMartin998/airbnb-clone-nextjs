3099c5ec61ffd8c27aae5afd6fad13ec939cfb28
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-09 18:23:56 -0500

    [UPD]: getListings updated to have more flexible query

--- src/airbnb/actions/getListings.ts
+++ src/airbnb/actions/getListings.ts
@@ -14,9 +14,36 @@ export const getListings = async (params: IListingsParams) => {
   } = params;
 
   try {
+    // build flexible query
     let query: any = {};
 
     if (userId) query.userId = userId;
+    if (category) query.category = category;
+    if (roomCount)
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    if (guestCount)
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    if (bathroomCount)
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    if (locationValue) query.locationValue = locationValue;
+
+    if (startDate && endDate)
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              { endDate: { gte: startDate }, startDate: { lte: startDate } },
+              { startDate: { lte: endDate }, endDate: { gte: endDate } },
+            ],
+          },
+        },
+      };
 
     const listings = await db.listing.findMany({
       where: query,
