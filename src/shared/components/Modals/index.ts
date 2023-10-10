20bc0b5da7d2def2fa090adab738313ef27225a0
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-09 19:58:46 -0500

    [ADD]: useSerachModal zustand

--- src/shared/components/Modals/index.ts
+++ src/shared/components/Modals/index.ts
@@ -1,4 +1,6 @@
 export { default as LoginModal } from './LoginModal/LoginModal';
 export { default as Modal } from './Modal/Modal';
 export { default as RegisterModal } from './RegisterModal/RegisterModal';
+export { default as BodyContent } from './RentModal/BodyContent';
 export { default as RentModal } from './RentModal/RentModal';
+export { default as SearchModal } from './SearchModal/SearchModal';
