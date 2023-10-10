52f45ad228f9db43ed235b1a40c518d5bb254a17
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-09 20:05:34 -0500

    [ADD]: Loader - Next.js

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Heading } from './Heading/Heading';
 export { default as Map } from './Map/Map';
+export { default as Loader } from './Loader/Loader';
 
 export * from './Modals';
 export * from './ui';
